f45bb15710aa7608810abe678f612b37700b9819
NurMukashev <mukashev.nur@gmail.com>
2023-12-26 13:49:13 +0600

    Add tags for filtering

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -8,6 +8,16 @@ export default{
     },
 
     template: `
+        <div class="flex gap-2">
+            <button
+                @click="currentTag = tag"
+                v-for="tag in tags"
+                class="border rounded px-1 py-px text-xs"
+                :class="{
+                    'border-blue-500 text-blue-500' : tag === currentTag
+                }"
+                >{{ tag }}</button>
+        </div>
         <assignment-list :assignments="filters.inProgress" title="InProgress"></assignment-list>
         <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
         
@@ -17,20 +27,33 @@ export default{
     data(){
       return {
         assignments: [
-            {name:'Finish the project', complete:false, id:1},
-            {name:'Meeting with partners', complete:false, id:2},
-            {name:'Hire a consultant', complete:false, id:3},
+            {name:'Finish the project', complete:false, id:1, tag:'science'},
+            {name:'Meeting with partners', complete:false, id:2, tag:'biology'},
+            {name:'Hire a consultant', complete:false, id:3, tag:'physics'},
         ],
+
+        currentTag: 'all',
       };
     },
 
     computed: {
         filters(){
             return {
-                inProgress: this.assignments.filter(assignment => !assignment.complete),
-                completed: this.assignments.filter(assignment => assignment.complete)
+                inProgress: this.filteredAssignments.filter(assignment => !assignment.complete),
+                completed: this.filteredAssignments.filter(assignment => assignment.complete)
             };
         },
+        filteredAssignments(){
+            if(this.currentTag === 'all'){
+                return this.assignments;
+            }
+
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        },
+
+        tags(){
+           return ['all', ...new Set(this.assignments.map(a => a.tag))];
+        }
     },
 
     methods: {
